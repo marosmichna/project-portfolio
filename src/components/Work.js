273f44e19365607754b1066d82f562be01c06398
Maroš Michna <maros24121990@gmail.com>
2022-11-16 11:34:30 +0100

    add Task-App project

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import WorkImg from "../images/WorkImg.jpg";
-import RealWork from "../images/RealWork.png"
-import NameAgeApp from "../images/name-age-app.png"
+import RealWork from "../images/RealWork.png";
+import NameAgeApp from "../images/name-age-app.png";
+import TaskApp from "../images/task-app.png"
 
 const Work = () => {
   return (
@@ -37,20 +38,20 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${RealWork})`}}
+                <div style={{backgroundImage: `url(${TaskApp})`}}
                     className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto container-div">
 
                     {/* Hover Effects */}
 
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className="text-2xl font-bold text-white tracking-wider drop-shadow-lg shadow-black">
-                            React JS Application
+                            Task App
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="/">
+                            <a href="https://task-immos59b0-marosmichna.vercel.app/">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/marosmichna/task-app">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                             </a>
                         </div>
