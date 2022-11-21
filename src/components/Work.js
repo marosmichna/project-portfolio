b4a2b3e3701d911f076915579ea1ed6b294c2b57
Maroš Michna <maros24121990@gmail.com>
2022-11-21 09:48:01 +0100

    update name-age-todo App

--- src/components/Work.js
+++ src/components/Work.js
@@ -29,10 +29,10 @@ const Work = () => {
                             Name-Age Todo App
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="name-age-app.vercel.app">
+                            <a href="name-age-931l2or26-marosmichna.vercel.app">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                             </a>
-                            <a href="name-age-931l2or26-marosmichna.vercel.app">
+                            <a href="https://github.com/marosmichna/name-age-app">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                             </a>
                         </div>
